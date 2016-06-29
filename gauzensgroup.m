a899c0806b96714f9b974e9c7e0afd1742ecd101
Kelly Kearney <kakearney@gmail.com>
2016-06-29 13:15:47 -0400

    Replaced trophic sim. calcs with call to trophicsimilarity function

--- gauzensgroup.m
+++ gauzensgroup.m
@@ -137,41 +137,8 @@ end
 % Trophic similarity
 
 if strcmp(Opt.type, 'trophicgroup')
-
-    npred = sum(adj,2);
-    nprey = sum(adj,1);
-    bprey = zeros(nnode);
-    bpred = zeros(nnode);
-    etsim = zeros(nnode);
     
-    tsim = zeros(nnode);
-   
-    for ii = 1:nnode
-        for jj = 1:nnode
-
-            % Observed
-            
-            tsim(ii,jj) = (sum(adj(:,ii) & adj(:,jj)) + sum(adj(ii,:) & adj(jj,:))) ./ ...
-                          (sum(adj(:,ii) | adj(:,jj)) + sum(adj(ii,:) | adj(jj,:)));
-                      
-            % Expected in random graph
-            
-            N = 0:min(npred([ii jj]));
-            n = 0:min(nprey([ii jj]));
-            [NN,nn] = meshgrid(N,n);
-            
-            hg1 = hygepdf(N, nnode, min(npred([ii,jj])), max(npred([ii,jj])));
-            hg2 = hygepdf(n, nnode, min(nprey([ii,jj])), max(nprey([ii,jj])));
-            [hg1, hg2] = meshgrid(hg1, hg2);
-            
-            tmp = ((NN + nn)./(npred(ii) + npred(jj) - NN + nprey(ii) + nprey(jj) - nn)) .* ...
-                    hg1 .* hg2; 
-            etsim(ii,jj) = nansum(tmp(:));
-
-        end
-    end
-    tsim(isnan(tsim)) = 0; % Occurs only for disconnected groups  
-    
+    [tsim, etsim] = trophicsimilarity(adj);
     [ir,ic] = meshgrid(1:nnode);
     
 end
