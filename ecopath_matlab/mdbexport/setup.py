fd708639b24abdf63f95bf86bee3b63b5b2ff074
Kelly Kearney <kakearney@gmail.com>
2017-02-13 12:51:14 -0800

    Deleting old dependencies

--- ecopath_matlab/mdbexport/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name='mdbexport',
-    version='0.1dev',
-    packages=['mdbexport'],
-    author='Kelly Kearney',
-    author_email='kakearney@gmail.com'
-)
